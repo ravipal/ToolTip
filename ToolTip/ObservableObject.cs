8afe82f2d3cf9bba70dcf3de0103967f0c98d761
Ravikumar Palanisamy <ravipal@microsoft.com>
2020-08-25 18:10:16 -0700

    Add project files.

---
+++ ToolTip/ObservableObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ToolTip
+{
+    public class ObservableObject : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler PropertyChanging;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetPropertyValue<T>(ref T property, T value, IEqualityComparer<T> comparer = null, [CallerMemberName] string propertyName = "")
+        {
+            if (!(comparer ?? EqualityComparer<T>.Default).Equals(property, value))
+            {
+                RaisePropertyChanging(propertyName);
+
+                property = value;
+
+                RaisePropertyChanged(propertyName);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        protected void RaisePropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ToolTip/ViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToolTip
+{
+    public class ViewModel : ObservableObject, INotifyDataErrorInfo
+    {
+        private string _selectedMode;
+        public ViewModel()
+        {
+            Modes = new HashSet<string>();
+            Modes.Add("Mode1");
+            Modes.Add("Mode2");
+            Modes.Add("Mode3");
+        }
+
+        public HashSet<string> Modes { get; private set; }
+
+        public string SelectedMode
+        {
+            get
+            {
+                return _selectedMode;
+            }
+            set
+            {
+                SetPropertyValue(ref _selectedMode, value);
+                Validate();
+            }
+        }
+
+        #region INotifyDataErrorInfo
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            // We only support a single validation error right now
+            if (string.Equals(propertyName, nameof(SelectedMode), StringComparison.OrdinalIgnoreCase))
+            {
+                if (SelectedMode.Equals("Mode2"))
+                {
+                    yield return new ValidationResult(false, new ValidationContent(isWarning: true, message: "The selected Mode is not valid"));
+                }
+            }
+        }
+
+        public bool HasErrors { get; private set; }
+
+        #endregion
+
+        private void Validate()
+        {
+            HasErrors = SelectedMode.Equals("Mode2");
+            this.ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(SelectedMode)));
+            RaisePropertyChanged(nameof(HasErrors));
+        }
+    }
+
+    public class ValidationContent
+    {
+        public ValidationContent(bool isWarning, string message)
+        {
+            IsWarning = isWarning;
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+
+        public bool IsWarning { get; private set; }
+    }
+}
